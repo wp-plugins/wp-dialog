015d91a4ae88f84b49c5be36a1579538b1c21424
zhangge <zhangge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-23 07:20:29 +0000

    FIX A BUG
    git-svn-id: https://plugins.svn.wordpress.org/wp-dialog/trunk@1031060 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scroll-bar.php
+++ scroll-bar.php
@@ -7,7 +7,7 @@
 <div id="gg">
 	<div class="close"><a href="javascript:void(0)" onclick="$('#gg').slideUp('slow');" title="关闭">×</a>
 	<div id="feedb"><a href="/feed" rel="nofollow" target="_blank" title="欢迎订阅我的博客" class="image"><img alt="订阅图标按钮" src="<?php echo plugins_url('images/feed.gif',__FILE__);?>" style="width:23px;height:23px;" /></a>
-<?php if(get_option('display_button')=="display"||get_option('display_button')==""){ ?>
+<?php if((get_option('display_button')=="display"||get_option('display_button')=="")&&(get_option('display_dialog')=="" || get_option('display_dialog')=="display")){ ?>
 	<a href="javascript:void(0)" onclick="deleteCookie('welcome');welcome();" title="呼出欢迎对话框"><img src="<?php echo plugins_url('skins/icons/face-happy.png',__FILE__);?>" style="width:23px;height:23px;"></a>
 <?php } ?>
 	</div>
